431b750a06438d691cfbd1130bbd06e1f4aa138b
David Byttow <david.byttow@gmail.com>
2020-08-26 22:16:25 -0400

    Initial commit

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterController2D : MonoBehaviour {
+
+	private const float groundCheckRadius = 0.2f;
+	private const float ceilingCheckRadius = 0.2f;
+	private const float gravity = -10;
+
+	[Range(0, 0.3f)] [SerializeField] private float movementSmoothing;
+	[SerializeField] private LayerMask groundMask;
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private Transform ceilingCheck;
+	[SerializeField] private float walkSpeed = 10f;
+	[SerializeField] private float walkAcceleration = 40f;
+	[SerializeField] private float groundDeceleration = 100f;
+	[SerializeField] private float inAirSpeedScale = 0.8f;
+	[SerializeField] private float jumpForce = 20f;
+	[SerializeField] private float fallingGravityScale = 2f;
+
+
+	private BoxCollider2D boxCollider;
+	private float inAirStartTime = 0;
+
+	private Vector3 velocity = Vector3.zero;
+	private bool isGrounded = true;
+
+	void Awake() {
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+	void Start() {
+		inAirStartTime = Time.time;
+	}
+
+	void Update() {
+		if (isGrounded) {
+			velocity.y = 0;
+		}
+
+		var falling = !isGrounded && velocity.y < 0;
+		var gravityScale = 1f; //falling ? fallingGravityScale : jumpingGravityScale;
+
+		if (!isGrounded && velocity.y < 0) {
+			gravityScale = fallingGravityScale;
+			Debug.Log("Falling");
+		}
+
+		velocity.y += Physics2D.gravity.y * gravityScale * Time.deltaTime;
+	
+		transform.Translate(velocity * Time.deltaTime);
+
+		CheckGround();
+	}
+
+	void FixedUpdate() {
+	}
+
+	private void CheckGround() {
+		bool wasGrounded = isGrounded;
+		isGrounded = false;
+
+		Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, groundMask);
+		foreach (Collider2D hit in hits) {
+			if (hit == boxCollider) {
+				continue;
+			}
+			ColliderDistance2D dist = hit.Distance(boxCollider);
+			if (dist.isOverlapped) {
+				transform.Translate(dist.pointA - dist.pointB);
+			}
+			if (Vector2.Angle(dist.normal, Vector2.up) < 90 && velocity.y < 0) {
+				isGrounded = true;
+			}
+		}
+
+		if (wasGrounded && !isGrounded) {
+			inAirStartTime = Time.time;
+		}
+
+		if (isGrounded && !wasGrounded) {
+			if (Time.time - inAirStartTime > 0.2f) {
+				// TODO: Sound fx
+			}
+		}
+	}
+
+	public void Move(float horizontalInput) {
+		if (horizontalInput != 0) {
+			velocity.x = Mathf.MoveTowards(velocity.x, walkSpeed * horizontalInput, walkAcceleration * Time.deltaTime);
+		} else {
+			velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
+		}
+	}
+
+	public void Jump() {
+		if (isGrounded) {
+			Debug.Log("JUMP");
+			// TODO: Sound fx
+			isGrounded = false;
+			velocity.y += jumpForce;
+			inAirStartTime = Time.time;
+		}
+	}
+
+	public void CancelJump() {
+		if (!isGrounded) {
+			var damping = velocity.y > 0 ? 0.5f : 1f;
+			velocity.y *= damping;
+		}
+	}
+
+	void OnDrawGizmosSelected() {
+		if (groundCheck) {
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class Player : MonoBehaviour {
+
+	[SerializeField] private ParticleSystem deathEffect;
+
+	private SpriteRenderer sprite;
+	private CharacterController2D controller;
+	private bool dead = false;
+	private bool jumping = false;
+
+	void Awake() {
+		sprite = GetComponent<SpriteRenderer>();
+		controller = GetComponent<CharacterController2D>();
+	}
+
+	void Start() {
+	}
+
+	void Update() {
+		bool jump = Input.GetButtonDown("Jump");
+		if (jump) {
+			jumping = true;
+			controller.Jump();
+		}
+		if (Input.GetButtonUp("Jump")) {
+			jumping = false;
+			controller.CancelJump();
+		}
+
+		float horizontalInput = Input.GetAxisRaw("Horizontal");
+		controller.Move(horizontalInput);
+	}
+
+	void FixedUpdate() {
+	}
+
+	public void Die() {
+		if (dead) {
+			return;
+		}
+
+		// TODO: Camera shake and sound FX
+
+		dead = true;
+
+		if (deathEffect) {
+			var effect = Instantiate(deathEffect, transform.position, Quaternion.Euler(0, -90, 0));
+			effect.Play();
+		}
+
+		gameObject.SetActive(false);
+		GameManager.global.QueueRestart();
+	}
+}
